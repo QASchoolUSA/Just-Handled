557a4ffe42d5bd380db1636aca17029c90c3235e
Nikita Kedrov <qaschoolusa@gmail.com>
2026-01-12 22:56:41 -0500

    Added new changes.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-2841612929-79b96",
-  "appId": "1:615239026186:web:4d4581ddce0b0b6e7801f3",
-  "apiKey": "AIzaSyBohwab_K8N0AVcsNNGwDrm1OgrQHTRLmI",
-  "authDomain": "studio-2841612929-79b96.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "615239026186"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -19,9 +19,7 @@ export function initializeFirebase() {
     } catch (e) {
       // Only warn in production because it's normal to use the firebaseConfig to initialize
       // during development
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
+      // Fallback to firebase config object
       firebaseApp = initializeApp(firebaseConfig);
     }
 
